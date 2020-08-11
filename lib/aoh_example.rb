1fad4f63bcb11a56b11174c379f01bfb43c5af3d
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-11 20:46:57 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [{:name => "Don Gately"
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
